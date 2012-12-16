e86a64408417e7e08845f9a383412107d8be1737
xwcoder <wangwei111516@gmail.com>
2012-12-16 19:51:23 +0800

    core.js add domready func

--- core.js
+++ core.js
@@ -34,7 +34,10 @@ Mix.extend = function ( t, s, defaults ) {
     var isIE6 = isIE && !window.XMLHttpRequest;
     var isIE7 = ua.indexOf( 'msie 7' ) > -1;
     var isIE8 = ua.indexOf( 'msie 7' ) > -1;
-    
+
+    var domready = false;
+    var readylist = [];
+
     Mix.extend( Mix, {
 
         isIE : isIE,
@@ -201,6 +204,85 @@ Mix.extend = function ( t, s, defaults ) {
                 return ( new obj() ).constructor;			
             }
             return obj;
+        },
+        
+        ready : function () {
+            var args = [].slice.call( arguments );
+            var i = 0, fn;
+
+            if ( domready ) {
+                while ( ( fn = args[ i++ ] ) ) {
+                    fn.call( window );
+                }
+            } else {
+                Mix.each( args, function ( fn ) {
+                    readylist.push( fn );
+                } );
+            }
         }
+    } );
+
+    /*!
+     * contentloaded.js
+     *
+     * Author: Diego Perini (diego.perini at gmail.com)
+     * Summary: cross-browser wrapper for DOMContentLoaded
+     * Updated: 20101020
+     * License: MIT
+     * Version: 1.2
+     *
+     * URL:
+     * http://javascript.nwbox.com/ContentLoaded/
+     * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+     *
+     */
+
+    // @win window reference
+    // @fn function reference
+    function contentLoaded(win, fn) {
+
+        var done = false, top = true,
+
+        doc = win.document, root = doc.documentElement,
+
+        add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+        rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+        pre = doc.addEventListener ? '' : 'on',
+
+        init = function(e) {
+            if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+            (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+            if (!done && (done = true)) fn.call(win, e.type || e);
+        },
+
+        poll = function() {
+            try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+            init('poll');
+        };
+
+        if (doc.readyState == 'complete') fn.call(win, 'lazy');
+        else {
+            if (doc.createEventObject && root.doScroll) {
+                try { top = !win.frameElement; } catch(e) { }
+                if (top) poll();
+            }
+            doc[add](pre + 'DOMContentLoaded', init, false);
+            doc[add](pre + 'readystatechange', init, false);
+            win[add](pre + 'load', init, false);
+        }
+
+    }
+
+    var fireReadyList = function () {
+        var i = 0, fn;            
+
+        while ( ( fn = readylist[ i++ ] ) ) {
+            fn.call( this );
+        }
+    };
+
+    contentLoaded( this, function () {
+        domready = true;    
+        fireReadyList();
     } );
 } )( this, this.undefined );
